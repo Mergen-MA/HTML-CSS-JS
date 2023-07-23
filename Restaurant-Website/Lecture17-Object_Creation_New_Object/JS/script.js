9268154a15d2f660f61e086a2109848a4d8870d3
Mergen-MA <malidinc13@gmail.com>
2023-07-23 08:39:36 +0200

    Creating objects using Obejct Literal Syntax

--- Restaurant-Website/Lecture17-Object_Creation_New_Object/JS/script.js
+++ Restaurant-Website/Lecture17-Object_Creation_New_Object/JS/script.js
@@ -1,18 +1,33 @@
 // Object creation
-var company = new Object();
-company.name = "Facebook"
-company.ceo = new Object();
-company.ceo.firstName = "Mali"
-company.ceo.favColour = "blue"
 
-console.log(company);
-console.log("Company CEO name is: " 
-+ company.ceo.firstName)
+// var company = new Object();
+// company.name = "Facebook"
+// company.ceo = new Object();
+// company.ceo.firstName = "Mali"
+// company.ceo.favColour = "blue"
 
-console.log(company["name"]);
+// console.log(company);
+// console.log("Company CEO name is: " 
+// + company.ceo.firstName)
 
-var stockPropName = "stock of company";
-company[stockPropName] = 110;
+// console.log(company["name"]);
 
-console.log("Stock price is = " + 
-company[stockPropName]);
+// var stockPropName = "stock of company";
+// company[stockPropName] = 110;
+
+// console.log("Stock price is = " + 
+// company[stockPropName]);
+
+// Better way: object literal
+
+var facebook = {
+    name: "Facebook",
+    ceo: {
+        firstName: "Mali",
+        favColor: "blue"
+    },
+    "stock of company": 110
+};
+
+console.log(facebook);
+
