42eeab8cd5d5fd30e300dcf28cbc122e24f4493c
Mergen-MA <malidinc13@gmail.com>
2023-07-23 19:05:45 +0200

    Creating arrays with long hand and short hand way

--- Restaurant-Website/Lecture20-Function_Constructors, Prototype&this/JS/script.js
+++ Restaurant-Website/Lecture20-Function_Constructors, Prototype&this/JS/script.js
@@ -48,3 +48,13 @@ console.log(myOtherCircle);
 
 
 
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype.bark = function() {
+    console.log(this.name + " likes barking! Bark!");
+}
+
+var max = new Dog("Max", "Buddy");
+max.bark();
