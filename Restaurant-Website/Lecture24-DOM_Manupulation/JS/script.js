19425ad951b3ae3c9ac8933f2e074a735f0f5cf4
Mergen-MA <malidinc13@gmail.com>
2023-07-25 09:09:09 +0200

    DOM Manupulation via Document property

---
+++ Restaurant-Website/Lecture24-DOM_Manupulation/JS/script.js
@@ -0,0 +1,29 @@
+// DOM Manipulation
+
+// console.log(document.getElementById('title'));
+// console.log(document instanceof HTMLDocument);
+
+function sayHello() {
+    var name =
+        document.getElementById("name").value;
+        var message = "<h2>Hello " + name +  "!</h2>";
+    
+    // document
+    //     .getElementById("content")
+    //     .textContent = message;  //Selector ayni sekilde yazilir ekrana
+
+    document
+    .getElementById("content")
+    .innerHTML = message;
+
+    if (name === "student") {
+        var title =
+         document
+            .querySelector("#title")
+            .textContent;
+        title += " & Loving' it!";
+        document 
+            .querySelector("h1")
+            .textContent = title;
+        }
+}
