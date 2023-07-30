a92ecf4d27841260d451aa536ab8034fd175d00f
Mergen-MA <malidinc13@gmail.com>
2023-07-30 07:05:20 +0200

    home-page snippet was embedded into the js code along with ajax-loading icon

--- Restaurant-Website/Lecture29-Dynamically Loading Home View Content/after/js/script.js
+++ Restaurant-Website/Lecture29-Dynamically Loading Home View Content/after/js/script.js
@@ -9,3 +9,42 @@ $(function () { // SAme as document.addEventListener("DOMContentLoaded"...
 
     });
 });
+
+(function (global) {
+
+    var dc = {};
+
+    var homeHtml = "snippets/home-snippet.html";
+
+    // Convinience function for inserting innerHTML for 'select'
+    var innertHtml = function (selector, html) {
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHtml = html;
+    };
+
+
+    // Show loading icon inside element identified by 'selector'.
+    var showLoading = function (selector) {
+        var html = "<div> class='text-center'>";
+        html += "<img src='images/ajax-loader.gif'></div>";
+        insertHtml(selector, html);
+    };
+
+    // On page load (before images or CSS)
+    document.addEventListener("DOMContentLoaded", function (event) {
+
+    // On first load, show home view
+    showLoading("#main-content");
+    $ajaxUtils.sendGetRequest(
+        homeHtml,
+        function (responseText){
+            document.querySelector("#main-content")
+            .innerHTML = responseText;
+            },
+        
+    false);
+});
+
+global.$dc = dc;
+
+})(window);
